12139e1c6e7a8fbaba35cb26d5e2e5c58c77d901
RadiaIdel <idelkadiradia@gmail.com>
2024-02-21 23:09:14 +0100

    front end of user profile

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -7,23 +7,28 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+
+
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
+                    @include('profile.partials.top-user-info')
+            </div>
+
+
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                     @include('profile.partials.update-profile-information-form')
-                </div>
             </div>
 
+            <!-- Section de modification du mot de passe -->
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
-                </div>
             </div>
 
+            <!-- Section de suppression de l'utilisateur -->
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
                     @include('profile.partials.delete-user-form')
-                </div>
             </div>
+
+
         </div>
     </div>
 </x-app-layout>
